08ce749e2dd0d814a67ca9569dcb570675e355ca
rahulalishetty <rahulalishetty1905@gmail.com>
2024-10-16 23:25:10 +0530

    style: fix

--- app/components/ui/Logo.tsx
+++ app/components/ui/Logo.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import LogoImg from "../../public/favicon.ico";
+import LogoImg from "@/public/favicon.ico";
 
 export default function Logo() {
   return (
